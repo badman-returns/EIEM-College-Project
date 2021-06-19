ad6738e0ce837f24749d373b218522fefed06b5c
tsgoswami <trishnangshugoswami@yahoo.in>
2021-06-19 19:35:29 +0530

    banner image css added in banner component in home page

--- frontend/src/main/pages/home/components/banner/banner.tsx
+++ frontend/src/main/pages/home/components/banner/banner.tsx
@@ -2,25 +2,40 @@ import React from 'react';
 import Carousel, { autoplayPlugin } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import imageOne from '../../../../../assets/college1.png';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+    bannerImg: {
+        width: '100%',
+        height: '100%',
+    }
+})
 
 const Banner: React.FC = () => {
+    const classes = useStyles();
     return (
         <>
-            <Carousel
-                plugins={[
-                    'infinite',
-                    {
-                        resolve: autoplayPlugin,
-                        options: {
-                            interval: 2000,
-                        }
-                    },
-                ]}
-                animationSpeed={1000}
-            >
-                <img src={imageOne} alt='bgimage1' />
-                <img src={imageOne} alt='bgimage1' />
-            </Carousel>
+            <Grid container justify='center' alignItems='center'>
+                <Grid item xs={12}>
+                    <Carousel
+                        plugins={[
+                            'infinite',
+                            {
+                                resolve: autoplayPlugin,
+                                options: {
+                                    interval: 2000,
+                                }
+                            },
+                        ]}
+                        animationSpeed={1000}
+                    >
+                        <img className={classes.bannerImg} src={imageOne} alt='bgimage1' />
+                        <img className={classes.bannerImg} src={imageOne} alt='bgimage1' />
+                    </Carousel>
+                </Grid>
+            </Grid>
         </>
     )
 }
