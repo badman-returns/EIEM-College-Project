ed414042728459226bf1cb296f2b0085e7473607
Pranjal <deypranjal123@gmail.com>
2021-06-25 18:58:58 +0530

    Message from director updated

--- frontend/src/main/pages/home/home.tsx
+++ frontend/src/main/pages/home/home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About from './components/about/about';
 import Banner from './components/banner/banner';
+import Message from './components/message/message';
 
 const HomePage:React.FC = () => {
     return(
         <>
         <Banner/>
         <About />
+        <Message/>
         </>
     )
 }
